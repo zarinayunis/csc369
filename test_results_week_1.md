e8213632ae98b85112749cd8b33b9098f4785d3e
Zarina Yunis <zarinayunis@gmail.com>
2025-01-13 15:35:37 -0800

    committing week 1 files

---
+++ test_results_week_1.md
@@ -0,0 +1,17 @@
+# Week 1 Results
+## 1-Hour Timeframe
+- **Timeframe:** 2022-04-01 12 to 2022-04-01 13
+- **Execution Time:** 666264.941542
+- **Most Placed Color:** #FFFFFF
+- **Most Placed Pixel Location:** (5,29)
+## 3-Hour Timeframe
+- **Timeframe:** 2022-04-02 01 to 2022-04-02 04
+- **Execution Time:** 659267.530125
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** (0,0)
+## 6-Hour Timeframe
+- **Timeframe:** 2022-04-02 01 to 2022-04-02 06
+- **Execution Time:** 656786.959458
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** (859,766)
+ 
---
+++ week1.py
@@ -0,0 +1,54 @@
+import csv
+from time import perf_counter_ns
+from collections import Counter
+from datetime import datetime
+
+file_path = "/Users/zzyun/Documents/College/Cal Poly/Year 4/Winter Quarter/CSC 369/2022_place_canvas_history.csv"
+
+def most_placed_color_and_pixel():
+    start_hour_str = input("What is the starting time in YYYY-MM-DD HH format? ")
+    end_hour_str = input("What is the ending time in YYYY-MM-DD HH format? ")
+
+    start_hour = datetime.strptime(start_hour_str, "%Y-%m-%d %H")
+    end_hour = datetime.strptime(end_hour_str, "%Y-%m-%d %H")
+
+    hex_codes_counter = Counter()
+    pixel_locations_counter = Counter()
+
+    start, end = None, None
+
+    start = perf_counter_ns()
+
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        next(reader)
+
+
+        for row in reader:
+            time = row[0]
+            try:
+                row_time = datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f %Z")
+
+                if start_hour <= row_time <= end_hour:
+                    hex_code = row[2]
+                    pixel_location = row[3]
+                    hex_codes_counter[hex_code] += 1
+                    pixel_locations_counter[pixel_location] += 1
+
+            except ValueError:
+                pass 
+
+    end = perf_counter_ns()
+
+    milliseconds = (end - start) / 1000000 
+
+    return hex_codes_counter.most_common(1), pixel_locations_counter.most_common(1), milliseconds
+
+
+
+def main():
+    color, pixel, milliseconds = most_placed_color_and_pixel()
+    print(color, pixel, milliseconds)
+
+if __name__ == "__main__":
+    main()
